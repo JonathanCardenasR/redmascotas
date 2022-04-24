288146832f75933612b5372ffc0a17f0cdb90c51
Jonathan Cardenas <greiff.jonathan@gmail.com>
2022-04-24 08:56:10 -0500

    rutas para crear veterinariazs

--- routers/index.js
+++ routers/index.js
@@ -2,9 +2,15 @@ const { Router } = require('express');
 
 const router = Router();
 
-const {getHome} = require('../controllers/home.controller')
+const homeController = require('../controllers/home.controller')
+const userController = require('../controllers/user.controller')
+const vetController = require('../controllers/vet.controller')
 
-router.get('/', getHome)
+router.get('/', homeController.getHome)
+router.get('/', homeController.getHome)
+
+router.get('/vet/create/:id', vetController.getCreateVet)
+router.post('/vet/create', vetController.createVet)
 
 module.exports = router;
 
