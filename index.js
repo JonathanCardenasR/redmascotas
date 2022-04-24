2704d9b48dd5635df754621a689a7f5a415d778f
Jhan Mendoza Aylas <20181173@aloe.ulima.edu.pe>
2022-04-24 12:19:54 -0500

    Barra de busqueda-headerRol para luego utilizar con el admi,usuario o default

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 var PORT = process.env.PORT || 8000
 
+const res = require('express/lib/response')
 const app = require('./app')
+
+
+
+//PaginaDeSesion
+app.get('/Sesion',(req, res) =>{
+    const textoRespuesta = "Sesion Iniciada"
+    res.render('home')
+})
+
+
 
 app.listen(PORT, ()=> {
     console.log(`El servidor se inicio correctamente en el puerto ${PORT}`)
