0668800c4a995ca4f047ef5191d3edf740207394
Arnold <arnoldag98@gmail.com>
2022-06-13 16:42:50 -0500

    update user v1

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var PORT = process.env.PORT || 8000
 
 const res = require('express/lib/response')
 const app = require('./app')
+const db = require('./model/models')
 
 app.set('views', __dirname + "/views")
 
@@ -28,4 +29,39 @@ app.get('/page',(req, res) =>{
 
 app.listen(PORT, ()=> {
     console.log(`El servidor se inicio correctamente en el puerto ${PORT}`)
+})
+
+// 
+app.get('/users', async (req, res)=> {
+    const timestampActual = new Date().getTime();
+    const dif = timestampActual - req.session.lastLogin
+
+    if (dif >= 3 * 60 * 60 * 1000) {
+        req.session.destroy() // Destruyes la sesion
+        res.render('/login')
+    }else {
+        // Obtener usuarios de la base de datos
+        const userss = await db.User.findAll({
+            order : [
+                ['id', 'DESC']
+            ]
+        });
+
+    }
+
+})
+//CRUDS  MODIFICAR
+app.get('/users/modificar/:codigo',(req, res) => {
+    const idUser = req.params.codigo
+
+    
+    const user = await db.User.findOne({
+        wher : {
+            id : idUser
+        }
+    })
+})
+
+app.get('/pets/modificar',(req, res) => {
+    
 })
